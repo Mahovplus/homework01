0ed960e173a69354bfb36ea3b0dfcfa4ff375e07
Mahovplus <mahovplus66@mail.ru>
2024-09-04 01:16:25 +0300

    Add files via upload

---
+++ Практическое задание- базовые структуры данных.py
@@ -0,0 +1,8 @@
+"1st program"
+print((9 ** 0.5) * 5)
+"2st program"
+print(9.99 >= 9.98 and 1000 != 1000.1)
+"3rd program"
+print(2 * 2 + 2 == 2 * (2 + 2))
+"4th program"
+print(int(float('123.456') * 10) / 10)
